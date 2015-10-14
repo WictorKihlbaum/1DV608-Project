7db39da86aebcc8707925747a3941e63883f5667
WictorKihlbaum <wk222as@student.lnu.se>
2015-10-14 15:08:12 +0200

    local server code update
    I can now connect to local server, and list username and password

--- model/LoginModel.php
+++ model/LoginModel.php
@@ -13,7 +13,75 @@ class LoginModel {
     }
 
     public function validateUserInput($user) {
-        
+		
+		$host = "127.0.0.1";
+		$port = 8889;
+		$socket = "8889";
+		$user = "root";
+		$password = "root";
+		$dbname = "mylocaldb";
+		
+		$con = new mysqli($host, $user, $password, $dbname, $port, $socket)
+			or die ('Could not connect to the database server' . mysqli_connect_error());
+			
+		if ($con) {
+			echo "Successful Connection!";
+		}
+		
+		$query = "SELECT userName, password FROM users";
+
+		if ($stmt = $con -> prepare($query)) {
+			
+			$stmt -> execute();
+			$stmt -> bind_result($userName, $password);
+			
+			while ($stmt -> fetch()) {
+				printf("%s %s\n", $userName, $password);
+    		}
+				
+			$stmt->close();
+		}
+		
+		$con -> close();
+		
+		
+		
+
+
+
+		
+		
+		
+		
+		
+		
+		/*while ($stmt->fetch()) {
+				
+		}*/
+		
+		
+		/*
+		$query = "SELECT userName, password FROM users";
+		$result = mysqli_query($con, $query) or die("No query");
+		
+		$users = array();
+		
+		while ($row = mysqli_fetch_assoc($result)) {
+			
+			$users[] = $row;
+		}
+		
+		echo $users;
+		
+		$con -> close();
+		*/
+		
+		
+
+
+		
+		
+        /*
         $inputToSearchFor = "Username: " . $user -> getUserName() . " Password: " . $user -> getPassword();
         $textFileToSearchIn = file_get_contents($this -> registeredUsersFile);
         $textFileToSearchIn = explode("\n", $textFileToSearchIn);
@@ -24,6 +92,7 @@ class LoginModel {
         }
         
         $this -> sessionModel -> setUserSession();
+		*/
     }
     
     public function logoutUser() {
