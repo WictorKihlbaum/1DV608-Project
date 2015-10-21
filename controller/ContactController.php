9d9d200db7ad3318f5c01881575ff9f4fc7abe6b
WictorKihlbaum <wk222as@student.lnu.se>
2015-10-21 22:24:36 +0200

    test

--- controller/ContactController.php
+++ controller/ContactController.php
@@ -23,43 +23,47 @@ class ContactController {
 			
 			if ($email != null) {
 				
-				/*$url = 'https://api.sendgrid.com/';
-				$user = 'azure_470730f159f6d62fbb429813ff905ef9@azure.com';
-				$pass = 'cv5J892HoHx15Nz';
+				 $url = 'https://api.sendgrid.com/';
+				 $user = 'azure_470730f159f6d62fbb429813ff905ef9@azure.com';
+				 $pass = 'cv5J892HoHx15Nz'; 
 				
-				$params = array(
-					'api_user'  => $user,
-					'api_key'   => $pass,
-					'to'        => 'wictor.kihlbaum@gmail.com',
-					'subject'   => 'testing from curl',
-					'html'      => 'testing body',
-					'text'      => 'testing body',
-					'from'      => 'example@sendgrid.com',
-				  );
+				 $params = array(
+					  'api_user' => $user,
+					  'api_key' => $pass,
+					  'to' => 'wictor.kihlbaum@gmail.com',
+					  'subject' => 'testing from curl',
+					  'html' => 'testing body',
+					  'text' => 'testing body',
+					  'from' => 'wictor.kihlbaum@gmail.com',
+				   );
 				
+				 $request = $url.'api/mail.send.json';
 				
-				$request =  $url.'api/mail.send.json';
+				 // Generate curl request
+				 $session = curl_init($request);
 				
-				// Generate curl request
-				$session = curl_init($request);
-				// Tell curl to use HTTP POST
-				curl_setopt ($session, CURLOPT_POST, true);
-				// Tell curl that this is the body of the POST
-				curl_setopt ($session, CURLOPT_POSTFIELDS, $params);
-				// Tell curl not to return headers, but do return the response
-				curl_setopt($session, CURLOPT_HEADER, false);
-				// Tell PHP not to use SSLv3 (instead opting for TLS)
-				curl_setopt($session, CURLOPT_SSLVERSION, CURL_SSLVERSION_TLSv1_2);
-				curl_setopt($session, CURLOPT_RETURNTRANSFER, true);
+				 // Tell curl to use HTTP POST
+				 curl_setopt ($session, CURLOPT_POST, true);
 				
-				// obtain response
-				$response = curl_exec($session);
-				curl_close($session);
+				 // Tell curl that this is the body of the POST
+				 curl_setopt ($session, CURLOPT_POSTFIELDS, $params);
 				
-				// print everything out
-				print_r($response);*/
+				 // Tell curl not to return headers, but do return the response
+				 curl_setopt($session, CURLOPT_HEADER, false);
+				 curl_setopt($session, CURLOPT_RETURNTRANSFER, true);
 				
-				$message = $email -> getMessage();
+				 // obtain response
+				 $response = curl_exec($session);
+				 curl_close($session);
+				
+				 // print everything out
+				 echo $response;
+				 print_r($response);
+				 
+				 
+				 
+				
+				/*$message = $email -> getMessage();
 				$to = 'wictor.kihlbaum@gmail.com';
 				$subject = $email -> getSubject();
 				$headers = 'From: '. $email -> getName() . '\r\n' .
@@ -77,7 +81,11 @@ class ContactController {
 				} else { 
 					
 					echo '<p>Something went wrong, go back and try again!</p>'; 
-				}		   
+				}	*/
+				
+				
+				
+					   
 			}
 		}
 	}
