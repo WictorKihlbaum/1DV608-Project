5da38869f695b6a7c0ee174a4a012dad707d2a75
WictorKihlbaum <wk222as@student.lnu.se>
2015-10-21 21:41:28 +0200

    test

--- controller/ContactController.php
+++ controller/ContactController.php
@@ -23,42 +23,41 @@ class ContactController {
 			
 			//if ($email != null) {
 				
-				$url = 'smtp.sendgrid.net/'; //'https://api.sendgrid.com/';
+				$url = 'https://api.sendgrid.com/';
 				$user = 'azure_470730f159f6d62fbb429813ff905ef9@azure.com';
-				$pass = 'cv5J892HoHx15Nz'; 
+				$pass = 'cv5J892HoHx15Nz';
 				
 				$params = array(
-					'api_user' => $user,
-					'api_key' => $pass,
-					'to' => 'wictor.kihlbaum@gmail.com',
-					'subject' => 'testing from curl',
-					'html' => 'testing body',
-					'text' => 'testing body',
-					'from' => 'testing body',
-				 );
+					'api_user'  => $user,
+					'api_key'   => $pass,
+					'to'        => 'wictor.kihlbaum@gmail.com',
+					'subject'   => 'testing from curl',
+					'html'      => 'testing body',
+					'text'      => 'testing body',
+					'from'      => 'example@sendgrid.com',
+				  );
 				
-				 $request = $url.'api/mail.send.json';
 				
-				 // Generate curl request
-				 $session = curl_init($request);
+				$request =  $url.'api/mail.send.json';
 				
-				 // Tell curl to use HTTP POST
-				 curl_setopt ($session, CURLOPT_POST, true);
+				// Generate curl request
+				$session = curl_init($request);
+				// Tell curl to use HTTP POST
+				curl_setopt ($session, CURLOPT_POST, true);
+				// Tell curl that this is the body of the POST
+				curl_setopt ($session, CURLOPT_POSTFIELDS, $params);
+				// Tell curl not to return headers, but do return the response
+				curl_setopt($session, CURLOPT_HEADER, false);
+				// Tell PHP not to use SSLv3 (instead opting for TLS)
+				curl_setopt($session, CURLOPT_SSLVERSION, CURL_SSLVERSION_TLSv1_2);
+				curl_setopt($session, CURLOPT_RETURNTRANSFER, true);
 				
-				 // Tell curl that this is the body of the POST
-				 curl_setopt ($session, CURLOPT_POSTFIELDS, $params);
+				// obtain response
+				$response = curl_exec($session);
+				curl_close($session);
 				
-				 // Tell curl not to return headers, but do return the response
-				 curl_setopt($session, CURLOPT_HEADER, false);
-				 curl_setopt($session, CURLOPT_RETURNTRANSFER, true);
-				
-				 // obtain response
-				 $response = curl_exec($session);
-				 curl_close($session);
-				
-				 // print everything out
-				 print_r($response);
-				 echo $response;
+				// print everything out
+				print_r($response);
 				
 				
 				/*
