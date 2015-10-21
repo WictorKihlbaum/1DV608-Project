2cfad9e2fd366d7caa99787979740d1f1ce48eb7
WictorKihlbaum <wk222as@student.lnu.se>
2015-10-21 22:38:37 +0200

    git

--- controller/ContactController.php
+++ controller/ContactController.php
@@ -21,49 +21,9 @@ class ContactController {
 			
 			$email = $this -> contactView -> getEmailContent();
 			
-			if ($email != null) {
+			if ($email != null) { 
 				
-				 $url = 'https://api.sendgrid.com/';
-				 $user = 'azure_470730f159f6d62fbb429813ff905ef9@azure.com';
-				 $pass = 'cv5J892HoHx15Nz'; 
-				
-				 $params = array(
-					  'api_user' => $user,
-					  'api_key' => $pass,
-					  'to' => 'wictor.kihlbaum@gmail.com',
-					  'subject' => 'testing from curl',
-					  'html' => 'testing body',
-					  'text' => 'testing body',
-					  'from' => 'wictor.kihlbaum@gmail.com',
-				   );
-				
-				 $request = $url.'api/mail.send.json';
-				
-				 // Generate curl request
-				 $session = curl_init($request);
-				
-				 // Tell curl to use HTTP POST
-				 curl_setopt ($session, CURLOPT_POST, true);
-				
-				 // Tell curl that this is the body of the POST
-				 curl_setopt ($session, CURLOPT_POSTFIELDS, $params);
-				
-				 // Tell curl not to return headers, but do return the response
-				 curl_setopt($session, CURLOPT_HEADER, false);
-				 curl_setopt($session, CURLOPT_RETURNTRANSFER, true);
-				
-				 // obtain response
-				 $response = curl_exec($session);
-				 curl_close($session);
-				
-				 // print everything out
-				 echo $response;
-				 print_r($response);
-				 
-				 
-				 
-				
-				/*$message = $email -> getMessage();
+				$message = $email -> getMessage();
 				$to = 'wictor.kihlbaum@gmail.com';
 				$subject = $email -> getSubject();
 				$headers = 'From: '. $email -> getName() . '\r\n' .
@@ -81,11 +41,7 @@ class ContactController {
 				} else { 
 					
 					echo '<p>Something went wrong, go back and try again!</p>'; 
-				}	*/
-				
-				
-				
-					   
+				}	   
 			}
 		}
 	}
